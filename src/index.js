fe6101e2b98874d93d171612ebcb0f3c45a8b910
João Lavoier <jmlavoier@hotmail.com>
2019-11-24 00:35:51 -0300

    Add custom hook

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
-const useClickout = () => {
+const defaultEvents = ['mousedown', 'touchstart'];
+
+const useClickout = (events = defaultEvents) => {
   const refWrap = useRef();
+  let handleClickout = () => {};
 
-  const handleClickout = () => {};
-
-  return {
-    refWrap,
-    handleClickout,
+  const handleClick = (event) => {
+    const isOut = !!refWrap.current && !refWrap.current.contains(event.target);
+    if (isOut) handleClickout();
   };
+
+  const bindClickout = (clickoutFn) => {
+    handleClickout = clickoutFn;
+  };
+
+  useEffect(() => {
+    events.forEach((event) => {
+      document.addEventListener(event, handleClick);
+    });
+
+    return () => {
+      events.forEach((event) => {
+        document.removeEventListener(event, handleClick);
+      });
+    };
+  });
+
+  return [refWrap, bindClickout];
 };
 
 export default useClickout;
