67a2cdf875bb4ce216b134bf5454c3fd45c94b12
João Lavoier <jmlavoier@hotmail.com>
2019-11-25 18:33:20 -0300

    Add JSDOM to test

--- setup.js
+++ setup.js
@@ -1,4 +1,29 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { JSDOM } from 'jsdom';
 
 configure({ adapter: new Adapter() });
+
+
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
+
+function copyProps(src, target) {
+  Object.defineProperties(target, {
+    ...Object.getOwnPropertyDescriptors(src),
+    ...Object.getOwnPropertyDescriptors(target),
+  });
+}
+
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js',
+};
+global.requestAnimationFrame = function requestAnimationFrame(callback) {
+  return setTimeout(callback, 0);
+};
+global.cancelAnimationFrame = function cancelAnimationFrame(id) {
+  clearTimeout(id);
+};
+copyProps(window, global);
